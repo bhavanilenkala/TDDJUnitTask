70ed0aa26f5885513ed80040df0835f05f49ee87
bhavanilenkala <49590346+bhavanilenkala@users.noreply.github.com>
2020-07-25 16:56:24 +0530

    Update RemoveAfromfirst2char.java

--- src/test/java/tasks/epam/JUnitTDDTask/RemoveAfromfirst2char.java
+++ src/test/java/tasks/epam/JUnitTDDTask/RemoveAfromfirst2char.java
@@ -14,37 +14,32 @@ public class RemoveAfromfirst2char {
      * "AABAA"=>"BAA"
     */
 	RemoveCharA removeAchar;
-	/*
+	
 	 @BeforeEach
 	void setUp() {
 		removeAchar=new RemoveCharA();
-	}*/
+	}
 	
 	@Test
 	public void testABCD() {
-		RemoveCharA removeAchar=new RemoveCharA();
 		assertEquals("BCD",removeAchar.removeCHAR("ABCD"));
 	}
 	
 	@Test
 	public void testAACD() {
-		RemoveCharA removeAchar=new RemoveCharA();
 		assertEquals("CD",removeAchar.removeCHAR("AACD"));
 	}
     
 	@Test
 	public void testBACD() {
-		RemoveCharA removeAchar=new RemoveCharA();
 		assertEquals("BCD",removeAchar.removeCHAR("BACD"));
 	}
 	@Test
 	public void testBBAA() {
-		RemoveCharA removeAchar=new RemoveCharA();
 		assertEquals("BBAA",removeAchar.removeCHAR("BBAA"));
 	}
 	@Test
 	public void testAABAA() {
-		RemoveCharA removeAchar=new RemoveCharA();
 		assertEquals("BAA",removeAchar.removeCHAR("AABAA"));
 	}
 	
